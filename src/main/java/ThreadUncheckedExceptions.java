087eef8049e14ae5062a43b1e5347b7f2b7d4ba4
BKD471 <bhaskarkumardas9@gmail.com>
2023-02-27 00:58:42 +0530

    Intro to threading

---
+++ src/main/java/ThreadUncheckedExceptions.java
@@ -0,0 +1,23 @@
+public class CreatingThread {
+
+    public static void main(String[] args) {
+        Thread t=new Thread(new Runnable() {
+            public void run() {
+                System.out.println("Now in thread "+Thread.currentThread().getName());
+                System.out.println("Priority "+Thread.currentThread().getPriority());
+                throw new RuntimeException();
+            }
+        });
+
+        t.setName("Worker Thread");
+        t.setPriority(Thread.MAX_PRIORITY);
+        t.setUncaughtExceptionHandler(new Thread.UncaughtExceptionHandler() {
+            @Override
+            public void uncaughtException(Thread t, Throwable e) {
+                System.out.println("An exception ocuured at"+t.getName()+"of "+e.getMessage());
+            }
+        });
+        t.start();
+
+    }
+}
